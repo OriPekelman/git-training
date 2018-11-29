d7eec3a3f63780d43217502e18c276881abc6b94
Ori Pekelman <ori@platform.sh>
2018-11-29 15:15:27 +0100

    make scenario reproducible

--- utilities/scenario.sh
+++ utilities/scenario.sh
@@ -15,20 +15,20 @@ git status
 echo "\n---Current directory structure of .git---\n"
 tree -C .git
 echo "\n---Commit file---\n"
-git commit -m"Added readme.md"
+GIT_AUTHOR_DATE="2018-10-11T06:06:51+01:00" GIT_COMMITTER_DATE=$GIT_AUTHOR_DATE git commit -m"Added readme.md"
 git status
 echo "\n---Modify file---\n"
-echo "\nNous avons appris aujourd'hui les commandes Git suivantes:\n\n1. `git init` - initialiser un nouveau dépôt git\n2. `git status` - connaître l'état du répeetoire de travail par rapport au dépôt git\n3. `git add` - ajouter des fichiers à l'index git pour préparer un commit\n4. `git commit -m"{message de commit}"` - sauvegarder un point d'étape dans le dépôt git\n" >> readme.md
+echo "\nNous avons appris aujourd'hui les commandes Git suivantes:\n\n1. \`git init\` - initialiser un nouveau dépôt git\n2. \`git status\` - connaître l'état du répeetoire de travail par rapport au dépôt git\n3. \`git add\` - ajouter des fichiers à l'index git pour préparer un commit\n4. \`git commit -m\"{message de commit}\"\` - sauvegarder un point d'étape dans le dépôt git\n" >> readme.md
 git status
 echo "\n---Add and commit file---\n"
 git add readme.md
 git status
-git commit -m"Add the list of commands we learnt today."
+GIT_AUTHOR_DATE="2018-10-11T06:10:30+01:00" GIT_COMMITTER_DATE=$GIT_AUTHOR_DATE git commit -m"Add the list of commands we learnt today."
 git status
 echo "\n---Create a second file---\n"
 echo "Git Example by OpenClassRooms\n\nTo the extent possible under law, the person who associated CC0 with\nGit Example  has waived all copyright and related or neighboring rights\nto Git Example.\n\nYou should have received a copy of the CC0 legalcode along with this\nwork.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>." > LICENSE
 git add LICENSE
-git commit -am"Adding a license file"
+GIT_AUTHOR_DATE="2018-10-11T06:15:42+01:00" GIT_COMMITTER_DATE=$GIT_AUTHOR_DATE git commit -am"Adding a license file"
 git status
 echo "\n---Current directory structure of .git---\n"
 tree -C .git
@@ -42,5 +42,5 @@ mkdir files
 git status
 touch files/.gitkeep
 git add files/.gitkeep
-git commit -m'add .gitkeep so files will be added to the repository'
+GIT_AUTHOR_DATE="2018-10-11T06:17:22+01:00" GIT_COMMITTER_DATE=$GIT_AUTHOR_DATE git commit -m'add .gitkeep so files will be added to the repository'
 git-objects-print-all
